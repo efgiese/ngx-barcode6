9b3fbe06de62d68623d8ce1fe315a03f84dbd897
Edgar Giese <edgar@egiese.de>
2018-10-26 20:16:12 +0200

    Fix testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
 
 import { NgxBarcode6Module } from 'projects/ngx-barcode6/src/lib/ngx-barcode6/ngx-barcode6.module';
 
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
         AppComponent
       ],
       imports: [
+        FormsModule,
         NgxBarcode6Module
       ]
     }).compileComponents();
@@ -20,15 +22,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'barcode'`, async(() => {
+  it(`should have as title 'ngx-barcode6'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('barcode');
+    expect(app.title).toEqual('ngx-barcode6');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to barcode!');
+    expect(compiled.querySelector('h1').textContent).toContain('ngx-barcode6');
   }));
 });
