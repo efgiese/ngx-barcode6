fe0ee3faeb5c95cd7b661ce8a5f27594fbd7d859
Edgar Giese <edgar@egiese.de>
2021-11-09 11:29:54 +0100

    rearrange entries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxBarcode6Module } from 'projects/ngx-barcode6/src/public-api';
 
 import { AppComponent } from './app.component';
-import { NgxBarcode6Module } from 'ngx-barcode6';
 
 @NgModule({
   declarations: [
