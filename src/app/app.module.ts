7f2ab8d833de2770d708e6094e401d765e73cf90
Edgar Giese <edgar@egiese.de>
2021-11-20 16:06:30 +0100

    fix path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxBarcode6Module } from 'projects/ngx-barcode6/src/public-api';
+import { NgxBarcode6Module } from 'ngx-barcode6';
 
 import { AppComponent } from './app.component';
 
