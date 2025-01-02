e7d5e40a250a276d2f4808973b28e6a72b84cd01
Edgar Giese <edgar@egiese.de>
2025-01-02 16:45:48 +0100

    Manuelle Anpassung

---
+++ projects/ngx-barcode6/src/lib/ngx-barcode6.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxBarcode6Component } from './ngx-barcode6.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxBarcode6Component
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxBarcode6Component
+  ]
+})
+export class NgxBarcode6Module { }
--- projects/ngx-barcode6/src/public-api.ts
+++ projects/ngx-barcode6/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/ngx-barcode6.component';
+export * from './lib/ngx-barcode6.module';
